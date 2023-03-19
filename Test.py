28b3a7f43443dfa0f85ccce1cf0ecccb1394ebe6
Saurabh Palande <85979667+Ronin998@users.noreply.github.com>
2023-03-19 11:13:25 +0530

    Add files via upload
    Files Uploaded for Data science

---
+++ Test.py
@@ -0,0 +1,16 @@
+#Strings
+
+contry = "India"
+
+print(contry)
+
+#Doc String actually used to put single and double quotes inside the strings 
+
+B= '''Saurabh's "Iphones"'''
+
+print(B)
+
+
+a = 'Pune'
+
+b= 'Mumbai'
